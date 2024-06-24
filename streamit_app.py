c8cd8135583df1d1de6c09dfbbe985cc7219be59
Daniel Acosta Imandt <danielacostadaf@gmail.com>
2024-06-24 11:14:38 -0600

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 #from snowflake.snowpark.context import get_active_session
 import pandas as pd
-
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -36,6 +36,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response= requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
 
        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen].iloc[0]
      # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
@@ -56,8 +59,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-import requests
+    
 
-fruityvice_response= requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
