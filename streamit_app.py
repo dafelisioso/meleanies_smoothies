95d77a3119c512e3c30e17de184cb84556f6603b
Daniel Acosta Imandt <danielacostadaf@gmail.com>
2024-06-24 11:36:10 -0600

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -16,7 +16,7 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col("Fruit_name"),coL('SEARCH_ON))
+my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col("Fruit_name"),coL('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
