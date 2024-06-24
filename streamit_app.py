fedb0d56816b9fe8646d3ba3f87f17da91d40855
Daniel Acosta Imandt <danielacostadaf@gmail.com>
2024-06-24 11:10:44 -0600

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -58,5 +58,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 import requests
 
-fruityvice_response= reqests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response= requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
